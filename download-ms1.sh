08e337baf560515b692d7171bafdb157db9d752c
Jon Ander Novella <jonandernovella@users.noreply.github.com>
2018-09-26 08:54:09 +0200

    Update download-ms1.sh

--- download-ms1.sh
+++ download-ms1.sh
@@ -11,5 +11,8 @@ prefix=X
 for i in $ms1names
 do 
 echo "Downloading ms1 file $i"
-curl -L https://www.ebi.ac.uk/metabolights/MTBLS558/files/${i#$prefix}?token=$token -o ms1/$i
+curl -L https://www.ebi.ac.uk/metabolights/MTBLS558/files/${i#$prefix}?token=$token -o ms1/$i.zip
+unzip ms1/$i.zip -d ./ms1/
 done
+
+rm -r ms1/*.zip
