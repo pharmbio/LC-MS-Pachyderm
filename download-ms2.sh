a558aaed2902ad8564f9f7cc0983ee7f360e7c25
Jon Ander Novella <jonandernovella@users.noreply.github.com>
2018-09-26 08:55:33 +0200

    Update download-ms2.sh

--- download-ms2.sh
+++ download-ms2.sh
@@ -10,5 +10,8 @@ ms2names=$(tail -n +2 params/ms2.csv)
 for i in $ms2names
 do 
 echo "Downloading ms2 file $i"
-curl -L https://www.ebi.ac.uk/metabolights/MTBLS558/files/${i}?token=$token -o ms2/$i
+curl -L https://www.ebi.ac.uk/metabolights/MTBLS558/files/${i}?token=$token -o ms2/$i.zip
+unzip ms2/$i.zip -d ./ms2/
 done
+
+rm -r ms2/*.zip
